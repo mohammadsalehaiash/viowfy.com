67c48ddc6de80c22188ba684728bf9dfc1b3c6c3
MDMDcode <mdmd.code@gmail.com>
2026-09-25 01:10:06 +0300

    cta

--- app/page.tsx
+++ app/page.tsx
@@ -17,9 +17,8 @@ export default function Page() {
         <Services />
         <Process />
         <WhyUs />
-         <CTA />
          <FAQ />
-
+        <CTA />
         {/* <Contact /> */}
       </main>
       <Footer />
